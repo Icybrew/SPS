b6407041ff80d3f31e9d2d0ffc455575dfea2064
Aurelijus <aurelijus.posius@gmail.com>
2019-06-06 19:09:27 +0300

    Created admin controller

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace SPS\Http\Controllers\admin;
+
+use SPS\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    
+    public function index()
+    {
+        return 'Admin index';
+    }
+    
+}
