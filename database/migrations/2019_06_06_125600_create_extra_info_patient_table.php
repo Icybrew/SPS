d661e827307ef7e0e89210743012ed60661e2603
Aurelijus <aurelijus.posius@gmail.com>
2019-06-06 16:03:22 +0300

    Created extra info patient migration

---
+++ database/migrations/2019_06_06_125600_create_extra_info_patient_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExtraInfoPatientTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('extra_info_patient', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('patient_id');
+            $table->foreign('patient_id')->references('id')->on('users');
+            $table->string('personal_identification_code');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('extra_info_patient');
+    }
+}
