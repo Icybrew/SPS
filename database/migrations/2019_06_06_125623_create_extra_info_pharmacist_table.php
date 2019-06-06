1ccb1b69d9bd9afaa658c191f4b70d09ce47f15f
Aurelijus <aurelijus.posius@gmail.com>
2019-06-06 16:04:12 +0300

    Created extra info pharmacist migration

---
+++ database/migrations/2019_06_06_125623_create_extra_info_pharmacist_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExtraInfoPharmacistTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('extra_info_pharmacist', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('pharmacist_id');
+            $table->foreign('pharmacist_id')->references('id')->on('users');
+            $table->string('workplace');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('extra_info_pharmacist');
+    }
+}
