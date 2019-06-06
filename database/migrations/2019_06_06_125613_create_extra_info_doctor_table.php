b8087f8d57211160221b29789a33e1e54f927f35
Aurelijus <aurelijus.posius@gmail.com>
2019-06-06 16:03:52 +0300

    Created extra info doctor migration

---
+++ database/migrations/2019_06_06_125613_create_extra_info_doctor_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExtraInfoDoctorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('extra_info_doctor', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('doctor_id');
+            $table->foreign('doctor_id')->references('id')->on('users');
+            $table->string('specialization');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('extra_info_doctor');
+    }
+}
