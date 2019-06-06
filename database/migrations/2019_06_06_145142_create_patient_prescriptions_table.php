4160ea8ce97d6a7b2163586f6255a3975edc3afe
Aurelijus <aurelijus.posius@gmail.com>
2019-06-06 18:38:04 +0300

    Created patient prescriptions migration

---
+++ database/migrations/2019_06_06_145142_create_patient_prescriptions_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePatientPrescriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patient_prescriptions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('doctor_id');
+            $table->foreign('doctor_id')->references('id')->on('users');
+            $table->unsignedBigInteger('patient_id');
+            $table->foreign('patient_id')->references('id')->on('users');
+            $table->unsignedBigInteger('medical_substance_id');
+            $table->foreign('medical_substance_id')->references('id')->on('medical_substances');
+            $table->string('description');
+            $table->timestamp('expires')->nullable();
+            $table->boolean('purchased')->default(0);
+            $table->timestamp('purchased_at')->nullable();
+            $table->unsignedBigInteger('pharmacist_id')->nullable();
+            $table->foreign('pharmacist_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patient_prescriptions');
+    }
+}
