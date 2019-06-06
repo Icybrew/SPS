3a19a72067fd16dc3f2c730204e2cde8412b1283
Aurelijus <aurelijus.posius@gmail.com>
2019-06-06 19:08:40 +0300

    Created admin middleware

---
+++ app/Http/Middleware/Admin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace SPS\Http\Middleware;
+
+use Closure;
+
+class Admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        //if (Auth::user() && Auth::user()->admin) {
+        //    return $next($request);
+        //}
+        
+        return $next($request);
+    }
+}
