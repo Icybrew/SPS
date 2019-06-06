822e7e769a61349934e0fabc05291470ffb785b9
Aurelijus <aurelijus.posius@gmail.com>
2019-06-06 17:50:57 +0300

    Created medical substances migration

---
+++ database/migrations/2019_06_06_135329_create_medical_substances_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMedicalSubstancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medical_substances', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->unique();
+            $table->double('substance_in_dose', 8, 2);
+            $table->unsignedBigInteger('measurement_unit_id');
+            $table->foreign('measurement_unit_id')->references('id')->on('measurement_units');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('medical_substances');
+    }
+}
