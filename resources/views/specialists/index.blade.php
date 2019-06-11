f7ea5af6ae88ee4b06127f24cf4c2a1bb0e75fbf
Aurelijus <aurelijus.posius@gmail.com>
2019-06-11 16:47:05 +0300

    Updated specialists view

--- resources/views/specialists/index.blade.php
+++ resources/views/specialists/index.blade.php
@@ -1,7 +1,203 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="container">
-    <div class="display-1 text-center">Specialists</div>
-</div>
+@component('components.header', ['name' => 'Specialists', 'link' => 'specialists.index'])
+@endcomponent
+<section class="team-area section_gap">
+    <div class="container">
+        <div class="row text-center mt-5">
+            <div class="col-lg-6 mx-auto my-5">
+                <h1 class="my-2 text-dark">Our Specialists</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+        </div>
+
+        <div class="row mb-5 justify-content-center d-flex align-items-center">
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 single-team mb-5">
+                <div class="thumb">
+                    <img class="img-fluid" src="img/t1.jpg" alt="">
+                    <div class="align-items-end justify-content-center d-flex">
+                        <div class="social-links">
+                            <a href="#">
+                                <i class="fab fa-facebook-f"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fab fa-twitter"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fas fa-basketball-ball"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fab fa-behance"></i>
+                            </a>
+                        </div>
+                        <h4>Andy Florence</h4>
+                        <p>Lorem ipsum...</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 single-team mb-5">
+                <div class="thumb">
+                    <img class="img-fluid" src="img/t2.jpg" alt="">
+                    <div class="align-items-end justify-content-center d-flex">
+                        <div class="social-links">
+                            <a href="#">
+                                <i class="fab fa-facebook-f"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fab fa-twitter"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fas fa-basketball-ball"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fab fa-behance"></i>
+                            </a>
+                        </div>
+                        <h4>Andy Florence</h4>
+                        <p>Lorem ipsum...</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 single-team mb-5">
+                <div class="thumb">
+                    <img class="img-fluid" src="img/t3.jpg" alt="">
+                    <div class="align-items-end justify-content-center d-flex">
+                        <div class="social-links">
+                            <a href="#">
+                                <i class="fab fa-facebook-f"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fab fa-twitter"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fas fa-basketball-ball"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fab fa-behance"></i>
+                            </a>
+                        </div>
+                        <h4>Andy Florence</h4>
+                        <p>Lorem ipsum...</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 single-team mb-5">
+                <div class="thumb">
+                    <img class="img-fluid" src="img/t4.jpg" alt="">
+                    <div class="align-items-end justify-content-center d-flex">
+                        <div class="social-links">
+                            <a href="#">
+                                <i class="fab fa-facebook-f"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fab fa-twitter"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fas fa-basketball-ball"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fab fa-behance"></i>
+                            </a>
+                        </div>
+                        <h4>Andy Florence</h4>
+                        <p>Lorem ipsum...</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 single-team mb-5">
+                <div class="thumb">
+                    <img class="img-fluid" src="img/t1.jpg" alt="">
+                    <div class="align-items-end justify-content-center d-flex">
+                        <div class="social-links">
+                            <a href="#">
+                                <i class="fab fa-facebook-f"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fab fa-twitter"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fas fa-basketball-ball"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fab fa-behance"></i>
+                            </a>
+                        </div>
+                        <h4>Andy Florence</h4>
+                        <p>Lorem ipsum...</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 single-team mb-5">
+                <div class="thumb">
+                    <img class="img-fluid" src="img/t2.jpg" alt="">
+                    <div class="align-items-end justify-content-center d-flex">
+                        <div class="social-links">
+                            <a href="#">
+                                <i class="fab fa-facebook-f"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fab fa-twitter"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fas fa-basketball-ball"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fab fa-behance"></i>
+                            </a>
+                        </div>
+                        <h4>Andy Florence</h4>
+                        <p>Lorem ipsum...</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 single-team mb-5">
+                <div class="thumb">
+                    <img class="img-fluid" src="img/t3.jpg" alt="">
+                    <div class="align-items-end justify-content-center d-flex">
+                        <div class="social-links">
+                            <a href="#">
+                                <i class="fab fa-facebook-f"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fab fa-twitter"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fas fa-basketball-ball"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fab fa-behance"></i>
+                            </a>
+                        </div>
+                        <h4>Andy Florence</h4>
+                        <p>Lorem ipsum...</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 single-team mb-5">
+                <div class="thumb">
+                    <img class="img-fluid" src="img/t4.jpg" alt="">
+                    <div class="align-items-end justify-content-center d-flex">
+                        <div class="social-links">
+                            <a href="#">
+                                <i class="fab fa-facebook-f"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fab fa-twitter"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fas fa-basketball-ball"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fab fa-behance"></i>
+                            </a>
+                        </div>
+                        <h4>Andy Florence</h4>
+                        <p>Lorem ipsum...</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 @endsection
