d91e0abdd17abecc5cb757a7ecaebf89ec17a809
Aurelijus <aurelijus.posius@gmail.com>
2019-06-14 13:47:28 +0300

    Renamed

--- database/migrations/2019_06_06_125613_create_extra_info_doctor_table.php
+++ database/migrations/2019_06_06_125613_create_extra_info_doctor_table.php
@@ -16,8 +16,9 @@ class CreateExtraInfoDoctorTable extends Migration
         Schema::create('extra_info_doctor', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedBigInteger('doctor_id');
-            $table->foreign('doctor_id')->references('id')->on('users')->onDelete('cascade');;
-            $table->string('specialization');
+            $table->foreign('doctor_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('specializations_id');
+            $table->foreign('specializations_id')->references('id')->on('specializations');
             $table->timestamps();
         });
     }
