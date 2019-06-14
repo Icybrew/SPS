50f6ac9fafc23b1a79e9fac75829f64ebf2f9a31
Aurelijus <aurelijus.posius@gmail.com>
2019-06-14 16:35:37 +0300

    Created admin config

---
+++ config/admin.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'paginate' => [
+        'doctors' => [
+            'index' => '9'
+        ],
+        'patients' => [
+            'index' => '9'
+        ],
+        'pharmacists' => [
+            'index' => '9'
+        ],
+        'admins' => [
+            'index' => '9'
+        ],
+    ],
+];
