6f194c712bed26920e37de1ede9f4c35080facbb
Aurelijus <aurelijus.posius@gmail.com>
2019-06-15 16:12:33 +0300

    Added pharmacists link

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -9,6 +9,9 @@
         <li class="nav-item">
             <a class="nav-link" href="{{ route('admin.patients.index') }}">Patients</a>
         </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('admin.pharmacists.index') }}">pharmacists</a>
+        </li>
     </ul>
 </section>
 @endsection
