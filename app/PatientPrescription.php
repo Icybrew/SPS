6101772b34519f836f89decae57e03bc80df451f
Aurelijus <aurelijus.posius@gmail.com>
2019-06-16 19:39:07 +0300

    Created PatientPrescription model

---
+++ app/PatientPrescription.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace SPS;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PatientPrescription extends Model
+{
+    protected $table = 'patient_prescriptions';
+
+    protected $fillable = [
+        'doctor_id',
+        'patient_id',
+        'medical_substance_id',
+        'description',
+        'expires_at',
+        'purchased',
+        'purchased_at',
+        'pharmacist_id',
+    ];
+
+    protected $casts = [
+        'expires_at' => 'datetime',
+        'purchased' => 'boolean',
+        'purchased_at' => 'datetime'
+    ];
+
+    public function doctor()
+    {
+        return $this->hasOne('SPS\User', 'id', 'doctor_id');
+    }
+
+    public function patient()
+    {
+        return $this->hasOne('SPS\User', 'id', 'patient_id');
+    }
+    
+    public function medicalSubstance()
+    {
+        return $this->hasOne('SPS\MedicalSubstance', 'id', 'medical_substance_id');
+    }
+
+}
