a9b58a67322236bb69dca84a8b21b718a328d48c
Aurelijus <aurelijus.posius@gmail.com>
2019-06-17 10:47:29 +0300

    Created user roles seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsersSeeder::class);
         $this->call(RolesSeeder::class);
+        $this->call(UserRolesSeeder::class);
         $this->call(SpecializationsSeeder::class);
         $this->call(MeasurementUnitsSeeder::class);
     }
---
+++ database/seeds/UserRolesSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserRolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(SPS\UserRole::class, 50)->create();
+    }
+}
