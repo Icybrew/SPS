be3d94c8870a503219ddbb6176199c8bd939cb88
Aurelijus <aurelijus.posius@gmail.com>
2019-06-17 10:40:47 +0300

    Updated user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,8 @@ use SPS\User;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 
+use Illuminate\Support\Facades\Hash;
+
 /*
 |--------------------------------------------------------------------------
 | Model Factories
@@ -18,10 +20,11 @@ use Faker\Generator as Faker;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        'firstname' => $faker->firstName,
+        'lastname' => $faker->lastName,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => Hash::make('1234'),
         'remember_token' => Str::random(10),
     ];
 });
