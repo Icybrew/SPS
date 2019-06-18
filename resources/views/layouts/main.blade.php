6bb298487df82e9d70b0bc8ce2504d1f3f1b8859
Aurelijus <aurelijus.posius@gmail.com>
2019-06-18 23:48:47 +0300

    Added content div

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -28,88 +28,88 @@
 
     </head>
     <body>
+        <div class="content">
+            <!-- Header -->
+            <nav class="navbar navbar-expand-md navbar-light bg-light shadow-sm">
+                <div class="container">
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        {{ config('app.name', 'SPS') }}
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
 
-        <!-- Header -->
-        <nav class="navbar navbar-expand-md navbar-light bg-light shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'SPS') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <!-- Left Side Of Navbar -->
+                        <ul class="navbar-nav mr-auto">
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+                        </ul>
 
-                    </ul>
+                        <!-- Middle of Navbar -->
+                        <ul class="nav navbar-nav center_nav pull-right">
+                            <li class="nav-item{{ Request::is('/') ? '  active' : NULL }}">
+                                <a class="nav-link" href="{{ route('home.index') }}">Home</a>
+                            </li>
+                            <li class="nav-item ">
+                                <a class="nav-link{{ Request::is('services*') ? ' active' : NULL }}" href="{{ route('services.index') }}">Services</a>
+                            </li>
+                            <li class="nav-item ">
+                                <a class="nav-link{{ Request::is('specialists*') ? ' active' : NULL }}" href="{{ route('specialists.index') }}">Specialists</a>
+                            </li>
+                            <li class="nav-item{{ Request::is('contacts*') ? ' active' : NULL }}">
+                                <a class="nav-link" href="{{ route('contacts.index') }}">Contacts</a>
+                            </li>
+    @if(Auth::check() && Auth::user()->isPatient())
+                            <li class="nav-item{{ Request::is('medical-history*') ? ' active' : NULL }}">
+                                <a class="nav-link" href="{{ route('medical-history.index') }}">Medical history</a>
+                            </li>
+    @endif
+    @if(Auth::check() && Auth::user()->isPatient())
+                            <li class="nav-item{{ Request::is('prescriptions*') ? ' active' : NULL }}">
+                                <a class="nav-link" href="{{ route('prescriptions.index') }}">Prescriptions</a>
+                            </li>
+    @endif
+    @if(Auth::check() && Auth::user()->hasRole(config('roles.name.admin')))
+                            <li class="nav-item{{ Request::is('admin*') ? ' active' : NULL }}">
+                                <a class="nav-link" href="{{ route('admin.index') }}">Admin</a>
+                            </li>
+    @endif
+                        </ul>
 
-                    <!-- Middle of Navbar -->
-                    <ul class="nav navbar-nav center_nav pull-right">
-                        <li class="nav-item{{ Request::is('/') ? '  active' : NULL }}">
-                            <a class="nav-link" href="{{ route('home.index') }}">Home</a>
-                        </li>
-                        <li class="nav-item ">
-                            <a class="nav-link{{ Request::is('services*') ? ' active' : NULL }}" href="{{ route('services.index') }}">Services</a>
-                        </li>
-                        <li class="nav-item ">
-                            <a class="nav-link{{ Request::is('specialists*') ? ' active' : NULL }}" href="{{ route('specialists.index') }}">Specialists</a>
-                        </li>
-                        <li class="nav-item{{ Request::is('contacts*') ? ' active' : NULL }}">
-                            <a class="nav-link" href="{{ route('contacts.index') }}">Contacts</a>
-                        </li>
-@if(Auth::check() && Auth::user()->isPatient())
-                        <li class="nav-item{{ Request::is('medical-history*') ? ' active' : NULL }}">
-                            <a class="nav-link" href="{{ route('medical-history.index') }}">Medical history</a>
-                        </li>
-@endif
-@if(Auth::check() && Auth::user()->isPatient())
-                        <li class="nav-item{{ Request::is('prescriptions*') ? ' active' : NULL }}">
-                            <a class="nav-link" href="{{ route('prescriptions.index') }}">Prescriptions</a>
-                        </li>
-@endif
-@if(Auth::check() && Auth::user()->hasRole(config('roles.name.admin')))
-                        <li class="nav-item{{ Request::is('admin*') ? ' active' : NULL }}">
-                            <a class="nav-link" href="{{ route('admin.index') }}">Admin</a>
-                        </li>
-@endif
-                    </ul>
+                        <!-- Right Side Of Navbar -->
+                        <ul class="navbar-nav ml-auto">
+                            <!-- Authentication Links -->
+                            @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->firstname }} <span class="caret"></span>
+                                </a>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                        @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->firstname }} <span class="caret"></span>
-                            </a>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
 
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                        @endguest
-                    </ul>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                            @endguest
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
-        <!-- Header End -->
+            </nav>
+            <!-- Header End -->
 
-        <!-- Main -->
-        <main>
-            @yield('content')
-        </main>
-        <!-- Main End -->
-
+            <!-- Main -->
+            <main>
+                @yield('content')
+            </main>
+            <!-- Main End -->
+        </div>
         <!-- Footer -->
         <footer class="footer-area shadow-sm p-5">
             <div class="container text-light my-5">
