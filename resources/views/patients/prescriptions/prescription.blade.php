528065b82eb9aeec386c155c740644d63dee1f43
Aurelijus <aurelijus.posius@gmail.com>
2019-06-19 23:46:49 +0300

    Created paients/prescriptions/prescription view

---
+++ resources/views/patients/prescriptions/prescription.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.main')
+
+@section('content')
+@component('components.header')
+{{ Breadcrumbs::render('patients.prescriptions.show', $patient, $prescription) }}
+@endcomponent
+<section class="container my-5">
+    <div class="col-xl-5 col-lg-6 col-md-8 col-sm-10 mx-auto">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">{{ $prescription->MedicalSubstance->name }}</h5>
+                <p class="card-text">{{ $prescription->description }}</p>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Substance per dose: {{ $prescription->substance_in_dose . ' ' . $prescription->measurementUnit->unit }}</li>
+                <li class="list-group-item">Doctor: {{ $prescription->doctor->firstname . ' ' . $prescription->doctor->lastname }}</li>
+                <li class="list-group-item">Expires at: {{ ($prescription->expires_at !== NULL) ? $prescription->expires_at->format('Y-m-d')  : 'Never' }}</li>
+            </ul>
+@if(count($prescription->purchases) > 0)
+            <div class="mt-3 mb-2 text-center">
+                <h4>Purchases</h4>
+            </div>
+            <ul class="list-group list-group-flush">
+@foreach($prescription->purchases as $purchase)
+                <li class="list-group-item">
+                    <div>
+                        Purchased at: {{ $purchase->purchased_at }}
+                    </div>
+                    <div>
+                        Sold by: {{ $purchase->pharmacist->firstname . ' ' . $purchase->pharmacist->lastname }}
+                    </div>
+                </li>
+@endforeach
+            </ul>
+@else
+            <div class="mt-3 mb-2 text-center">
+                <h4>Prescription is not yet purchased</h4>
+            </div>
+@endif
+        </div>
+        <div class="mt-3">
+            <a href="{{ route('patients.prescriptions.index', $patient->id) }}" class="mr-auto"><button type="button" class="btn btn-dark">&larr; All prescriptions</button></a>
+        </div>
+    </div>
+</section>
+@endsection
