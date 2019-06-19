d09f0d616cfd0cd644a46d9676839abf8f78ec2d
Aurelijus <aurelijus.posius@gmail.com>
2019-06-19 23:51:40 +0300

    Added patients, patient, patient medical history & prescriptions breadcrumbs

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -31,6 +31,7 @@ Breadcrumbs::register('contacts', function ($breadcrumbs) {
 });
 
 
+
 // Profile
 Breadcrumbs::register('profile', function ($breadcrumbs) {
     $breadcrumbs->parent('home');
@@ -47,4 +48,58 @@ Breadcrumbs::register('profile.edit', function ($breadcrumbs, $user) {
 Breadcrumbs::register('profile.change-password', function ($breadcrumbs, $user) {
     $breadcrumbs->parent('profile');
     $breadcrumbs->push('Change password', route('profile.edit-password', $user->id));
+});
+
+
+
+// Patients
+Breadcrumbs::register('patients', function ($breadcrumbs) {
+    $breadcrumbs->parent('home');
+    $breadcrumbs->push('Patients', route('patients.index'));
+});
+
+// Patients -> Patient
+Breadcrumbs::register('patients.show', function ($breadcrumbs, $patient) {
+    $breadcrumbs->parent('patients');
+    $breadcrumbs->push($patient->firstname . ' ' . $patient->lastname, route('patients.show', ['id' => $patient->id]));
+});
+
+
+
+// Patients -> Patient -> Medical history
+Breadcrumbs::register('patients.medical-history.index', function ($breadcrumbs, $patient) {
+    $breadcrumbs->parent('patients.show', $patient);
+    $breadcrumbs->push('Medical history', route('patients.medical-history.index', $patient->id));
+});
+
+// Patients -> Patient -> Medical history -> Entry
+Breadcrumbs::register('patients.medical-history.show', function ($breadcrumbs, $patient, $entry) {
+    $breadcrumbs->parent('patients.medical-history.index', $patient);
+    $breadcrumbs->push('Entry #' . $entry->id, route('patients.medical-history.show', [$patient->id, $entry->id]));
+});
+
+// Patients -> Patient -> Medical history -> New entry
+Breadcrumbs::register('patients.medical-history.create', function ($breadcrumbs, $patient) {
+    $breadcrumbs->parent('patients.medical-history.index', $patient);
+    $breadcrumbs->push('New entry', route('patients.medical-history.create', $patient->id));
+});
+
+
+
+// Patients -> Patient -> Prescriptions
+Breadcrumbs::register('patients.prescriptions.index', function ($breadcrumbs, $patient) {
+    $breadcrumbs->parent('patients.show', $patient);
+    $breadcrumbs->push('Prescriptions', route('patients.prescriptions.index', ['id' => $patient->id]));
+});
+
+// Patients -> Patient -> Prescriptions -> Entry
+Breadcrumbs::register('patients.prescriptions.show', function ($breadcrumbs, $patient, $prescription) {
+    $breadcrumbs->parent('patients.prescriptions.index', $patient);
+    $breadcrumbs->push('Prescription #' . $prescription->id, route('patients.prescriptions.show', ['patient_id' => $patient->id, 'prescription_id' => $prescription->id]));
+});
+
+// Patients -> Patient -> Prescriptions -> New prescription
+Breadcrumbs::register('patients.prescriptions.create', function ($breadcrumbs, $patient) {
+    $breadcrumbs->parent('patients.prescriptions.index', $patient);
+    $breadcrumbs->push('New prescription', route('patients.prescriptions.create', ['patient_id' => $patient->id]));
 });
