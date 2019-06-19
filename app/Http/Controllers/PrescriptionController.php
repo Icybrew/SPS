06462cf997e92eba532ab7a92864d00b47df76b8
Aurelijus <aurelijus.posius@gmail.com>
2019-06-19 23:50:30 +0300

    Updated prescription controller

--- app/Http/Controllers/PrescriptionController.php
+++ app/Http/Controllers/PrescriptionController.php
@@ -6,10 +6,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 use SPS\User;
-use SPS\MeasurementUnit;
-use SPS\MedicalSubstance;
 use SPS\PatientPrescription;
-use SPS\PatientPrescriptionPurchase;
+use SPS\MedicalSubstance;
+use SPS\MeasurementUnit;
 
 class PrescriptionController extends Controller
 {
@@ -18,11 +17,22 @@ class PrescriptionController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index($patient_id = NULL)
     {
-        // Getting prescriptions with medical substances and measurement units
-        $prescriptions = User::with(['prescriptions.doctor', 'prescriptions.patient', 'prescriptions.medicalSubstance', 'prescriptions.medicalSubstance.measurementUnit'])->find(Auth::user()->id)->prescriptions()->orderByRaw('-expires_at asc')->paginate();
-        return view('prescriptions.index', compact('prescriptions'));
+        // Checking if request if for specific patient
+        if ($patient_id !== NULL) {
+            // Getting patient with prescription relation
+            $patient = User::with(['prescriptions.doctor', 'prescriptions.patient', 'prescriptions.medicalSubstance', 'prescriptions.measurementUnit'])->findOrFail($patient_id);
+
+            // Getting prescription list from patient
+            $prescriptions = $patient->prescriptions()->orderByRaw('-expires_at asc')->paginate();
+
+            return view('patients.prescriptions.index', compact('patient', 'prescriptions'));
+        } else {
+            $prescriptions = User::with(['prescriptions.doctor', 'prescriptions.patient', 'prescriptions.medicalSubstance', 'prescriptions.medicalSubstance.measurementUnit'])->find(Auth::user()->id)->prescriptions()->orderByRaw('-expires_at asc')->paginate();
+
+            return 'WIP';
+        }
     }
 
     /**
@@ -31,11 +41,15 @@ class PrescriptionController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($patient_id, $prescription_id)
     {
-        // Getting prescription with medical substance and measurement unit
-        $prescription = PatientPrescription::with(['doctor', 'patient', 'medicalSubstance', 'medicalSubstance.measurementUnit', 'purchases', 'purchases.pharmacist'])->findOrFail($id);
-        return view('prescriptions.prescription', compact('prescription'));
+        // Getting specific prescription with relations
+        $prescription = PatientPrescription::with(['doctor', 'patient', 'medicalSubstance', 'measurementUnit', 'purchases', 'purchases.pharmacist'])->findOrFail($prescription_id);
+
+        // Getting patient from prescription relation
+        $patient = $prescription->patient;
+
+        return view('patients.prescriptions.prescription', compact('patient', 'prescription'));
     }
 
     /**
@@ -43,9 +57,20 @@ class PrescriptionController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create($patient_id)
     {
-        //
+        // Getting user with extraInfoPatient who has patient role
+        $patient = User::with('extraInfoPatient')->whereHas('roles', function ($q) {
+            $q->where('name', config('roles.name.patient'));
+        })->where('id', '=', $patient_id)->firstOrFail();
+
+        // Getting all medical substances
+        $substances = MedicalSubstance::all();
+
+        // Getting all measurement units
+        $measurementUnits = MeasurementUnit::all();
+
+        return view('patients.prescriptions.create', compact('patient', 'substances', 'measurementUnits'));
     }
 
     /**
@@ -54,9 +79,31 @@ class PrescriptionController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, $patient_id)
     {
-        //
+        // Validating data
+        $request->validate([
+            'expiration' => 'sometimes|nullable|date',
+            'medicine' => 'required|numeric|exists:medical_substances,id',
+            'amount' => 'required|numeric',
+            'unit' => 'required|numeric|exists:measurement_units,id',
+            'description' => 'required|string'
+        ]);
+
+        // Creating new prescription
+        $prescription = new PatientPrescription;
+
+        $prescription->doctor_id = Auth::user()->id;
+        $prescription->patient_id = $patient_id;
+        $prescription->medical_substance_id = $request->medicine;
+        $prescription->substance_in_dose = $request->amount;
+        $prescription->measurement_unit_id = $request->unit;
+        $prescription->description = $request->description;
+
+        // Saving new prescription
+        $prescription->save();
+        
+        return redirect()->route('patients.prescriptions.index', $patient_id)->with('success', 'New prescription added');
     }
 
     /**
