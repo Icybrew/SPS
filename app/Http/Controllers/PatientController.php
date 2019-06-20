a5545df6ac9d0aede317535012d471ff71c2c0dd
Aurelijus <aurelijus.posius@gmail.com>
2019-06-20 20:28:26 +0300

    Added authorizations & comments

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -13,11 +13,23 @@ use SPS\Role;
 
 class PatientController extends Controller
 {
+    
+    public function __construct() {
+
+        // Making sure user is logged in.
+        $this->middleware('auth');
+
+    }
 
     public function index(Request $request)
     {
+        // Checking if current user can view all patients
+        $this->authorize('viewPatients', User::class);
+
+        // Checking if we got search request
         if ($request->has('search')) {
 
+            // Searching for patients by given input
             $patients = User::select('users.*')->whereHas('roles', function ($q) { $q->where('name', '=', config('roles.name.patient')); })
                     ->leftJoin('extra_info_patient', 'users.id', '=', 'extra_info_patient.patient_id')
                     ->where(function ($q) use ($request) {
@@ -26,63 +38,80 @@ class PatientController extends Controller
                         $q->orWhere('ssn', 'LIKE', "%{$request->search}%");
                     })
                     ->orderBy('users.id', 'desc')
-                    ->paginate();
+                    ->paginate(2);
 
+            // Appending search input
             $patients->appends(['search' => $request->search]);
 
+            // Returning view with patients
             return view('patients.index', compact('patients'));
+
         } else {
+
             // Getting all users with patient role
             $patients = User::whereHas('roles', function ($q) { $q->where('name', '=', config('roles.name.patient')); })->orderBy('users.id', 'desc')->paginate();
 
+            // Returning view with patients
             return view('patients.index', compact('patients'));
         }
-
-
-        // Getting all users with patient role
-        $patients = Role::where('name', '=', config('roles.name.patient'))->first()->users()->orderBy('created_at', 'desc')->paginate();
-
-        return view('patients.index', compact('patients'));
     }
 
     public function show($id)
-    {
+    {        
         // Getting user with extraInfoPatient who has patient role
         $patient = User::with('extraInfoPatient')->whereHas('roles', function ($q) {
             $q->where('name', config('roles.name.patient'));
         })->where('id', '=', $id)->firstOrFail();
 
-        // Authorizing action
+        // Checking if current user can view patient
         $this->authorize('view', $patient);
 
+        // Returning view with patient
         return view('patients.patient', compact('patient'));
     }
 
     public function search(Request $request)
     {
+        // Checking if current user can search patients
+        $this->authorize('search', Patient::class);
         
+        // Validating data
         $request->validate([
             'search' => 'sometimes|required|numeric|digits:11'
         ]);
-        
+
+        // Checking if we got search request
         if ($request->has('search')) {
+
+            // Searching for patients by given SSN
             $patients = User::select('users.*')->whereHas('roles', function ($q) { $q->where('name', '=', config('roles.name.patient')); })
                     ->leftJoin('extra_info_patient', 'users.id', '=', 'extra_info_patient.patient_id')
                     ->where(function ($q) use ($request) {
                         $q->Where('extra_info_patient.ssn', 'LIKE', "%{$request->search}%");
                     })
                     ->paginate();
+            
+            // Appending search input
+            $patients->appends(['search' => $request->search]);
 
+            // Returning view with patients
             return view('patients.search', compact('patients'));
         } else {
+
+            // Returning view
             return view('patients.search');
         }
     }
 
     public function myPatients(Request $request)
     {
+        // Checking if current user can view they're patients
+        $this->authorize('viewPatients', User::class);
 
+        // Checking if we got search request
         if ($request->has('search')) {
+
+            // Searching for patients by given input
             $patients = Auth::user()->patients()->with('extraInfoPatient')
                     ->leftJoin('extra_info_patient', 'users.id', '=', 'extra_info_patient.patient_id')
                     ->where(function ($q) use ($request) {
@@ -93,17 +122,26 @@ class PatientController extends Controller
                     ->orderBy('users.id', 'desc')
                     ->paginate();
 
+            // Appending search input
             $patients->appends(['search' => $request->search]);
 
+            // Returning view with patients
             return view('patients.my-patients.index', compact('patients'));
         } else {
+
+            // Getting all users with patient role
             $patients = Auth::user()->patients()->with('extraInfoPatient')->orderBy('users.id', 'desc')->paginate();
+
+            // Returning view with patients
             return view('patients.my-patients.index', compact('patients'));
         }
     }
    
     public function myPatientsExport()
     {
+        // Checking if current user can export they're patients
+        $this->authorize('export', Patient::class);
+        
         // Getting current users patient list
         $patients = Auth::user()->patients()->with('extraInfoPatient')->paginate();
 
@@ -113,7 +151,7 @@ class PatientController extends Controller
             // File name to save as
             $fileName = 'patientInfoExports.txt';
 
-            // Deleting old file if exists.
+            // Deleting old file if it exists.
             if (file_exists($fileName)) {
                 unlink($fileName);
             }
@@ -129,8 +167,11 @@ class PatientController extends Controller
             // Closing file
             fclose($file);
 
+            // Returning file
             return response()->download($fileName);
         } else {
+
+            // Returning redirect back with errors
             return redirect()->back()->withErrors(new MessageBag(['export' => "You have 0 patients"]));
         }
     }
